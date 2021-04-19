b7daa115c36bed4693365714903be40ef1ae0ae8
arikarantce <46425400+arikarantce@users.noreply.github.com>
2021-04-19 16:18:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def link_handler(bot, message):
     
     
 async def get_shortlink(link): 
-    url = 'https://tamillyrics.co/ad/api'
+    url = 'https://wowlinks.me/api'
     params = {'api': API_KEY, 'url': link}
     
     async with aiohttp.ClientSession() as session:
